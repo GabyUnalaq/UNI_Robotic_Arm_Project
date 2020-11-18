03c4cb742b99bce99f1792ae3833bd81effe2fc2
GabyUnalaq <gaby_unalaq@yahoo.com>
2020-11-18 18:09:08 +0200

    Add files via upload

---
+++ src/Robotic_Arm_Project.cpp
@@ -0,0 +1,24 @@
+/***************************
+
+Main project for the Robotic Arm
+	4LF493
+	
+Programmers:
+ - Iordache Andrei
+ - Ittu Ioan
+ - Patrania Bogdan
+ - Rusu Catalin
+ - Terzian Tigran
+ - Tomuta Gabriel
+
+ ***************************/
+
+
+#include "Robotic_Arm_Project.h"
+
+
+int main()
+{
+	std::cout << "Buna siara!";
+	
+}
---
+++ src/Robotic_Arm_Project.h
@@ -0,0 +1,11 @@
+/***************************
+
+Main header for the project
+
+ ***************************/
+
+
+#include <iostream.h>
+
+#include "../include/AX12A.h"
+#include "../include/AX12A.cpp"
