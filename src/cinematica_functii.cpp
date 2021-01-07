28e66a6cbb6192a2fd484dff484f98e167b28aec
GabyUnalaq <gaby_unalaq@yahoo.com>
2021-01-07 18:51:39 +0200

    Update and rename calculare_miscare_robot.cpp to cinematica_functii.cpp

--- src/cinematica_functii.cpp
+++ src/cinematica_functii.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <cmath>
 #include <stdlib.h>
 
 #define pi 3.14159265358979323846
@@ -14,11 +15,14 @@ float rad_to_degree(float angle);
 
 float** calc_matrix(float angle, float dist);
 float** multiply_matrix(float ** a, float ** b);
+float* multiply_matrix(float ** mat, float * vec);
 
 void afisare_matrice(float** matrix);
 
-/// Programul principal
-int main(){
+/// CINEMATICA DIRECTA
+void cinematic(){
+    std::cout << std::endl;
+    std::cout << "========== Cinematica directa ==========" << std::endl << std::endl;
     float angle_1 = 150;
     float angle_2 = 150;
     float angle_3 = 150;
@@ -47,6 +51,79 @@ int main(){
 
     std::cout << "Matrice finala: " << std::endl;
     afisare_matrice(final_matrix);
+}
+
+/// CINEMATICA INVERSA
+void rev_cinematic(){
+    std::cout << std::endl;
+    std::cout << "========== Cinematica inversa ==========" << std::endl << std::endl;
+    /// Input:
+    float efx = 8; // effector x pos
+    float efy = 19; // effector y pos
+    float efa = 0; // effector angle with Ox
+
+    float d0 = 4.5; // distance from base to first joint
+    float d1 = 9.5; // distance from first to second joint
+    float d2 = 9.8; // distance from second to third joint
+    float d3 = 6.0; // distance from third to effector joint
+
+    /// Other variables
+    float x0 = 0, y0 = 0; // coords of the base
+    float x1, y1; // coords of the first joint
+    float x2, y2; // coords of the second joint
+    float x3, y3; // coords of the third joint
+
+    float dist; // distance to the effector
+    float a0, a1, a2, a3; // angles of the robot
+
+    /// Calculations:
+    x1 = x0;
+    y1 = y0 + d0;
+
+    x3 = -1 * d3 * cos(efa) + efx;
+    y3 = -1 * d3 * sin(efa) + efy;
+
+    dist = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+
+    a0 = rad_to_degree(atan((y1 - y3)/(x1 - x3)));
+    a1 = acos((d1*d1 + dist*dist - d2*d2)/(2 * d1 * dist)) + degree_to_rad(a0);
+    if (x3 <= 0)
+        a1 += 180;
+    a1 = rad_to_degree(a1);
+
+    a2 = rad_to_degree(acos((d1*d1 + d2*d2 - dist*dist)/(2 * d1 * d2))) + 180;
+
+    if ((a1 + a2 - efa) >= 360)
+        a3 = 360.0 - (a1 + a2 - efa - 360.0);
+    else
+        a3 = 360.0 - (a1 + a2 - efa);
+
+    x2 = d1 * cos(degree_to_rad(a1)) + x1;
+    y2 = d1 * sin(degree_to_rad(a1)) + y1;
+
+    /// TODO transformare
+
+    std::cout << "Pentru efectorul ce are: " << std::endl;
+    std::cout << "x = " << efx << ", y = " << efy << std::endl;
+    std::cout << "si orientarea " << efa << std::endl;
+    std::cout << std::endl;
+    std::cout << "Unghiurile cuplelor sunt:" << std::endl;
+    std::cout << "a1 = " << a1 << std::endl;
+    std::cout << "a2 = " << a2 << std::endl;
+    std::cout << "a3 = " << a3 << std::endl;
+    std::cout << std::endl;
+    std::cout << "Pozitiile cuplelor sunt:" << std::endl;
+    std::cout << "J1: ("  << x1 << ';' << y1 << ')'<< std::endl;
+    std::cout << "J2: ("  << x2 << ';' << y2 << ')'<< std::endl;
+    std::cout << "J3: ("  << x3 << ';' << y3 << ')'<< std::endl;
+    std::cout << std::endl;
+
+}
+
+/// MAIN
+int main(){
+    cinematic();
+    rev_cinematic();
 
     return 0;
 }
@@ -138,6 +215,16 @@ float** multiply_matrix(float ** a, float ** b){
         }
     }
     return mul;
+}
+
+/// Face inmultirea a unei matrici de 3/3 cu una 3/1, returnand rezultatul ca o matrice de 3/1
+float* multiply_matrix(float ** mat, float * vec){
+    float* vecpy = new float[3];
+    for (int i=0; i<3; i++)
+        vecpy[i] = vec[i];
+    for (int i=0; i<3; i++)
+        vec[i] = vecpy[0] * mat[i][0] + vecpy[1] * mat[i][1] + vecpy[2] * mat[i][2];
+    return vec;
 }
 
 /// Afiseaza o matrice de 3/3
